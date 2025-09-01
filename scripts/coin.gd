c711e75fdf047923eb1e85a314c8696b8d611db6
wessleyn <wessleynyakaz@gmail.com>
2025-09-01 12:06:36 +0200

    Added Coin Pickup Sound

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
 func _on_body_entered(body: Node2D) -> void:
 	print("+1 coin")
-	queue_free() # remove coin from scene
+	animation_player.play("pickup")
