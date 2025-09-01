915737f6d8b111c69b97c23cb6aab6731f242f18
wessleyn <wessleynyakaz@gmail.com>
2025-09-01 13:11:19 +0200

    persisted high school during a session

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,25 +1,27 @@
 extends CanvasLayer
 
 @onready var score_label: Label = $ScoreLabel
-@onready var game_over: Label = $GameOver
 
 var score = 0
 var coins = 0
-var HighestScore = 0
 
-func updateScore (score, coin, HighestScore):
-	score_label.text = "Highest Score: " + str(HighestScore) + "\nScore: " + str(score)  +  "\nCoins: " + str(coins) 
+func _ready():
+	 # initialize the current high score on game start
+	updateScore()
+	
+func updateScore ():
+	score_label.text = "Highest Score: " + str(Global.high_score) + "\nScore: " + str(score)  +  "\nCoins: " + str(coins) 
 	
 func addPoint():
 	score += 1
-	if score >= HighestScore:
+	if score >= Global.high_score:
 		setHighestScore()
 		
-	updateScore(score, coins, HighestScore)
+	updateScore()
 
 func addCoin():
 	coins += 1
 	addPoint()
 	
 func setHighestScore():
-	HighestScore = score
+	Global.high_score = score
