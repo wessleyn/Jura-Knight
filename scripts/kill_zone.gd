3e427e3bef7e67ecbfb548de76037be66103e1ab
wessleyn <wessleynyakaz@gmail.com>
2025-09-01 10:20:06 +0200

    created an enemy, slime

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -2,10 +2,13 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(knight: Node2D) -> void:
 	print("You died!")
+	Engine.time_scale = 0.5
+	knight.get_node("CollisionShape2D").queue_free()
 	timer.start() # create a delay before the game end
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene() # reload the game
