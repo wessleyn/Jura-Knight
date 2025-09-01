e0b94c5d3879c4a0f512f286339d5f5d2ceb2d6b
wessleyn <wessleynyakaz@gmail.com>
2025-09-01 10:27:32 +0200

    Added direction change to the knight
    Knight can now move his head according to the direction, giving him the ability to face where he's headed

--- scripts/knight.gd
+++ scripts/knight.gd
@@ -4,19 +4,25 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
+		
+	# Get the Knight direction -1 (left)  and 1 (right)
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip The Knight Direction
+	if direction > 0: # moving to the right
+		animated_sprite.flip_h = false # move to the left
+	elif direction < 0: # vice versa
+		animated_sprite.flip_h = true 
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
